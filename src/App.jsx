982d89beef12c68dac9b1a15cfb5149ab8f258b9
MSRossii <marcelosrossi13@gmail.com>
2024-07-02 23:23:47 -0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,59 @@ import image3 from "./assets/imgs/img3.jpg";
 import image4 from "./assets/imgs/img4.jpg";
 
 export default function App() {
+  //step 1: get DOM
+  let nextDom = document.getElementById("next");
+  let prevDom = document.getElementById("prev");
+
+  let carouselDom = document.querySelector(".carousel");
+  let SliderDom = carouselDom.querySelector(".carousel .list");
+  let thumbnailBorderDom = document.querySelector(".carousel .thumbnail");
+  let thumbnailItemsDom = thumbnailBorderDom.querySelectorAll(".item");
+  let timeDom = document.querySelector(".carousel .time");
+
+  thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+  let timeRunning = 3000;
+  let timeAutoNext = 7000;
+
+  nextDom.onclick = function () {
+    showSlider("next");
+  };
+
+  prevDom.onclick = function () {
+    showSlider("prev");
+  };
+  let runTimeOut;
+  let runNextAuto = setTimeout(() => {
+    next.click();
+  }, timeAutoNext);
+  function showSlider(type) {
+    let SliderItemsDom = SliderDom.querySelectorAll(".carousel .list .item");
+    let thumbnailItemsDom = document.querySelectorAll(
+      ".carousel .thumbnail .item"
+    );
+
+    if (type === "next") {
+      SliderDom.appendChild(SliderItemsDom[0]);
+      thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+      carouselDom.classList.add("next");
+    } else {
+      SliderDom.prepend(SliderItemsDom[SliderItemsDom.length - 1]);
+      thumbnailBorderDom.prepend(
+        thumbnailItemsDom[thumbnailItemsDom.length - 1]
+      );
+      carouselDom.classList.add("prev");
+    }
+    clearTimeout(runTimeOut);
+    runTimeOut = setTimeout(() => {
+      carouselDom.classList.remove("next");
+      carouselDom.classList.remove("prev");
+    }, timeRunning);
+
+    clearTimeout(runNextAuto);
+    runNextAuto = setTimeout(() => {
+      next.click();
+    }, timeAutoNext);
+  }
   return (
     <>
       <div>
@@ -117,9 +170,11 @@ export default function App() {
           </div>
 
           <div className="arrows">
-            <button className="prev"><</button>
-            <button className="next">></button>
+            <button className="prev">left</button>
+            <button className="next">right</button>
           </div>
+
+          <div class="time"></div>
         </div>
       </div>
     </>
