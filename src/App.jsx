586bc20156eab45783c958591c0cec5424311a80
MSRossii <marcelosrossi13@gmail.com>
2024-07-02 22:34:53 -0300

    create items

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,8 @@
+import image1 from "./assets/imgs/img1.jpg";
+import image2 from "./assets/imgs/img2.jpg";
+import image3 from "./assets/imgs/img3.jpg";
+import image4 from "./assets/imgs/img4.jpg";
+
 export default function App() {
   return (
     <>
@@ -10,6 +15,28 @@ export default function App() {
             <a href="">Info</a>
           </nav>
         </header>
+
+        {/* carousel */}
+        <div className="carousel">
+          <div className="list">
+            <div className="item">
+              <img src={image1} alt="" />
+              <div className="content">
+                <div className="author">LUNDEV</div>
+                <div className="title">Desing Slider</div>
+                <div className="topic">Game</div>
+                <div className="des">
+                  dnwqjndjwq dkjwnqjdwq jdnqwjndjq wndqwjndwq ndwq dnwqnd jwqndj
+                  wqn
+                </div>
+                <div className="buttons">
+                  <button>SEE MORE</button>
+                  <button>SUBSCRIBE</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
